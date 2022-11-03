0b6fecabdf0b470d6529638483bfb1c426ddf257
Jimmy Rozo <jimmyrozo29@gmail.com>
2022-11-03 00:34:31 -0500

    creation headar and sidebar - home

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   hide = false;
 
-  constructor() {}
+  constructor(
+    private route: Router
+  ) {}
 
   ngOnInit(): void {}
 
